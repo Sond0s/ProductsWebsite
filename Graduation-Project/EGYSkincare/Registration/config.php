00243ae3a6197a7f47a866fb3aa46a48a24be471
Sondos Khalifa <119331450+Sond0s@users.noreply.github.com>
2024-05-06 06:58:21 +0300

    Delete Graduation-Project/EGYSkincare directory

--- Graduation-Project/EGYSkincare/Registration/config.php
+++
@@ -1,8 +0,0 @@
-<!-- Connect login-signin with database table -->
-<?php 
-session_start();
-define ('SERVER' , 'localhost');
-define('USERNAME', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'skincareproject');
-?>
